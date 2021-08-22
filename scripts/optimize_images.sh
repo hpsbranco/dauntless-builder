4847966245653054f106c156344ce3a32ca36e72
Christopher Kaster <me@atomicptr.de>
2021-08-22 09:34:22 +0200

    Add missing Thunderdeep Drask data, fixes #194 (#195)

--- scripts/optimize_images.sh
+++ scripts/optimize_images.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+CONVERT="convert"
+
+# on windows there ImageMagick convert is called "imconvert" instead of "convert"
+if [[ "$OSTYPE" == "win32" || "$OSTYPE" == "msys" ]]; then
+    CONVERT="imconvert"
+fi
+
 function optimize_path () {
     for file in $(find $1 -name '*.png')
     do
         if [[ $(identify -format "%wx%h" $file) != "128x128" ]]; then
             # resize to 128px height
-            convert -resize 128x128 -gravity center -extent 128x128 -background none "$file" "$file"
+            $CONVERT -resize 128x128 -gravity center -extent 128x128 -background none "$file" "$file"
         fi
 
         # optmize images
