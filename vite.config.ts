5675c5029724eaeb56dd777f95f9c60091d3f48f
Christopher Kaster <me@atomicptr.de>
2022-05-28 09:33:55 +0200

    lint: make eslint check root directory

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,21 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
 import ViteFaviconsPlugin from "vite-plugin-favicon";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig(({ command, mode }) => {
     const isBuild = command === "build";
     const isDevMode = mode === "development";
 
     return {
+        build: {
+            assetsDir: "assets",
+            minify: !isDevMode,
+            outDir: "dist",
+            sourcemap: isDevMode,
+        },
         define: {
             DB_DEVMODE: isDevMode,
-        },
-        build: {
-            minify: !isDevMode,
-            sourcemap: isDevMode,
-            outDir: "dist",
-            assetsDir: "assets",
         },
         plugins: [
             react(),
