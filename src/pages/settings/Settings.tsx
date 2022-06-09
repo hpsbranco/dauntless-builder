012449b42c04808bca87f21035e1003fcc5f7875
Christopher Kaster <me@atomicptr.de>
2022-06-09 17:47:07 +0200

    make settings page a bit prettier

--- src/pages/settings/Settings.tsx
+++ src/pages/settings/Settings.tsx
@@ -1,4 +1,18 @@
-import { Checkbox, FormControl, FormControlLabel, InputLabel, MenuItem, Select } from "@mui/material";
+import { Build } from "@mui/icons-material";
+import {
+    Card,
+    CardContent,
+    FormControl,
+    InputLabel,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+    ListSubheader,
+    MenuItem,
+    Select,
+    Switch,
+} from "@mui/material";
 import PageTitle from "@src/components/PageTitle";
 import { selectConfiguration, setDevMode, setLanguage } from "@src/features/configuration/configuration-slice";
 import { useAppDispatch, useAppSelector } from "@src/hooks/redux";
@@ -15,39 +29,52 @@ const Settings: React.FC = () => {
         <>
             <PageTitle title={t("pages.settings.title")} />
 
-            <FormControl
-                fullWidth
-                sx={{ mt: 4 }}
-            >
-                <InputLabel>{t("pages.settings.language")}</InputLabel>
-                <Select
-                    label={t("pages.settings.language")}
-                    onChange={ev => dispatch(setLanguage(ev.target.value as Language))}
-                    value={currentLanguage()}
-                >
-                    {Object.keys(Language).map((key: string) => (
-                        <MenuItem
-                            key={key}
-                            value={Language[key as keyof typeof Language]}
+            <ListSubheader sx={{ mt: 2 }}>{t("pages.settings.general")}</ListSubheader>
+            <Card>
+                <CardContent>
+                    <FormControl
+                        fullWidth
+                        sx={{ mt: 1 }}
+                    >
+                        <InputLabel>{t("pages.settings.language")}</InputLabel>
+                        <Select
+                            label={t("pages.settings.language")}
+                            onChange={ev => dispatch(setLanguage(ev.target.value as Language))}
+                            value={currentLanguage()}
                         >
-                            {getNativeLanguageName(Language[key as keyof typeof Language]) !== null
-                                ? `${getNativeLanguageName(Language[key as keyof typeof Language])} (${key})`
-                                : key}
-                        </MenuItem>
-                    ))}
-                </Select>
-            </FormControl>
+                            {Object.keys(Language).map((key: string) => (
+                                <MenuItem
+                                    key={key}
+                                    value={Language[key as keyof typeof Language]}
+                                >
+                                    {getNativeLanguageName(Language[key as keyof typeof Language]) !== null
+                                        ? `${getNativeLanguageName(Language[key as keyof typeof Language])} (${key})`
+                                        : key}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                </CardContent>
+            </Card>
 
-            <FormControlLabel
-                control={
-                    <Checkbox
-                        checked={configuration.devMode}
-                        disabled={DB_DEVMODE}
-                        onChange={ev => dispatch(setDevMode(ev.target.checked))}
-                    />
-                }
-                label={t("pages.settings.dev-mode-toggle")}
-            />
+            <ListSubheader>{t("pages.settings.misc")}</ListSubheader>
+            <Card>
+                <CardContent>
+                    <List sx={{ p: 0 }}>
+                        <ListItem>
+                            <ListItemIcon>
+                                <Build />
+                            </ListItemIcon>
+                            <ListItemText primary={t("pages.settings.dev-mode-toggle")} />
+                            <Switch
+                                checked={configuration.devMode}
+                                disabled={DB_DEVMODE}
+                                onChange={ev => dispatch(setDevMode(ev.target.checked))}
+                            />
+                        </ListItem>
+                    </List>
+                </CardContent>
+            </Card>
         </>
     );
 };
