7b26be3bd65d4482d0273e296df033bd83141ba3
Christopher Kaster <me@atomicptr.de>
2022-08-03 19:14:38 +0200

    fix bug that caused meta builds cache to not be cleared properly

--- src/pages/build/MetaBuilds.tsx
+++ src/pages/build/MetaBuilds.tsx
@@ -58,53 +58,57 @@ const MetaBuilds: React.FC = () => {
 
     const { weaponType, buildCategoryIndex, showNote } = useAppSelector(selectMetaBuildsSelection);
 
-    const builds = useCache("metabuilds-builds", () => {
-        type WeaponBuilds = {
-            [categoryName: string]: BuildCategory;
-        };
+    const builds = useCache(
+        "metabuilds-builds",
+        () => {
+            type WeaponBuilds = {
+                [categoryName: string]: BuildCategory;
+            };
 
-        const builds = {
-            [WeaponType.AetherStrikers]: {} as WeaponBuilds,
-            [WeaponType.Axe]: {} as WeaponBuilds,
-            [WeaponType.ChainBlades]: {} as WeaponBuilds,
-            [WeaponType.Hammer]: {} as WeaponBuilds,
-            [WeaponType.Repeater]: {} as WeaponBuilds,
-            [WeaponType.Sword]: {} as WeaponBuilds,
-            [WeaponType.WarPike]: {} as WeaponBuilds,
-        };
+            const builds = {
+                [WeaponType.AetherStrikers]: {} as WeaponBuilds,
+                [WeaponType.Axe]: {} as WeaponBuilds,
+                [WeaponType.ChainBlades]: {} as WeaponBuilds,
+                [WeaponType.Hammer]: {} as WeaponBuilds,
+                [WeaponType.Repeater]: {} as WeaponBuilds,
+                [WeaponType.Sword]: {} as WeaponBuilds,
+                [WeaponType.WarPike]: {} as WeaponBuilds,
+            };
 
-        for (const category of metaBuildsJson.categories) {
-            for (const buildData of category.builds) {
-                const build = BuildModel.tryDeserialize(buildData.buildId);
+            for (const category of metaBuildsJson.categories) {
+                for (const buildData of category.builds) {
+                    const build = BuildModel.tryDeserialize(buildData.buildId);
 
-                const weaponType = build.data.weapon?.type;
+                    const weaponType = build.data.weapon?.type;
 
-                if (!weaponType) {
-                    continue;
+                    if (!weaponType) {
+                        continue;
+                    }
+
+                    if (!(category.name in builds[weaponType])) {
+                        builds[weaponType][category.name] = {
+                            builds: [],
+                            description: category.description,
+                            index: category.index,
+                            name: category.name,
+                            subcategoryDescription: category.subcategoryDescription ?? {},
+                            tier: category.tier ?? null,
+                        };
+                    }
+
+                    builds[weaponType][category.name].builds.push({
+                        buildId: build.serialize(),
+                        subcategory: buildData.subcategory ?? null,
+                        title: buildData.title,
+                        vsElement: buildData.vsElement as ElementalType | null,
+                    });
                 }
-
-                if (!(category.name in builds[weaponType])) {
-                    builds[weaponType][category.name] = {
-                        builds: [],
-                        description: category.description,
-                        index: category.index,
-                        name: category.name,
-                        subcategoryDescription: category.subcategoryDescription ?? {},
-                        tier: category.tier ?? null,
-                    };
-                }
-
-                builds[weaponType][category.name].builds.push({
-                    buildId: build.serialize(),
-                    subcategory: buildData.subcategory ?? null,
-                    title: buildData.title,
-                    vsElement: buildData.vsElement as ElementalType | null,
-                });
             }
-        }
 
-        return builds;
-    });
+            return builds;
+        },
+        [metaBuildsJson],
+    );
 
     const hasBuilds = useCallback(
         (category: string): boolean =>
@@ -146,146 +150,154 @@ const MetaBuilds: React.FC = () => {
         }
     }, [buildCategoryIndex, weaponType, hasBuilds, dispatch]);
 
-    const renderBuild = (index: number, buildId: string, title: string) => (
-        <Box key={index}>
-            <LazyLoadComponent
-                placeholder={
-                    <Skeleton
-                        height={300}
-                        variant={"rectangular"}
-                        width="100%"
-                    />
-                }
-            >
-                <Box>
-                    <BuildCard
-                        buildId={buildId}
-                        title={t(`pages.metabuilds.generated.buildTitles.${title}`)}
-                    />
-                </Box>
-            </LazyLoadComponent>
-        </Box>
+    const renderBuild = useCallback(
+        (index: number, buildId: string, title: string) => (
+            <Box key={index}>
+                <LazyLoadComponent
+                    placeholder={
+                        <Skeleton
+                            height={300}
+                            variant={"rectangular"}
+                            width="100%"
+                        />
+                    }
+                >
+                    <Box>
+                        <BuildCard
+                            buildId={buildId}
+                            title={t(`pages.metabuilds.generated.buildTitles.${title}`)}
+                        />
+                    </Box>
+                </LazyLoadComponent>
+            </Box>
+        ),
+        [t],
     );
 
-    const renderBuildsByElement = (builds: BuildCategoryBuild[]) => {
-        const buildsByElement = {
-            [ElementalType.Blaze]: [] as BuildCategoryBuild[],
-            [ElementalType.Frost]: [] as BuildCategoryBuild[],
-            [ElementalType.Terra]: [] as BuildCategoryBuild[],
-            [ElementalType.Shock]: [] as BuildCategoryBuild[],
-            [ElementalType.Radiant]: [] as BuildCategoryBuild[],
-            [ElementalType.Umbral]: [] as BuildCategoryBuild[],
-            None: [] as BuildCategoryBuild[],
-        };
+    const renderBuildsByElement = useCallback(
+        (builds: BuildCategoryBuild[]) => {
+            const buildsByElement = {
+                [ElementalType.Blaze]: [] as BuildCategoryBuild[],
+                [ElementalType.Frost]: [] as BuildCategoryBuild[],
+                [ElementalType.Terra]: [] as BuildCategoryBuild[],
+                [ElementalType.Shock]: [] as BuildCategoryBuild[],
+                [ElementalType.Radiant]: [] as BuildCategoryBuild[],
+                [ElementalType.Umbral]: [] as BuildCategoryBuild[],
+                None: [] as BuildCategoryBuild[],
+            };
 
-        builds.forEach(build => {
-            const element = build.vsElement ?? "None";
-            buildsByElement[element].push(build);
-        });
+            builds.forEach(build => {
+                const element = build.vsElement ?? "None";
+                buildsByElement[element].push(build);
+            });
 
-        return (
-            <>
-                {buildsByElement[ElementalType.Blaze].length > 0 && (
-                    <>
-                        <ListSubheader>{t("pages.metabuilds.vsElement.Blaze")}</ListSubheader>
+            return (
+                <>
+                    {buildsByElement[ElementalType.Blaze].length > 0 && (
+                        <>
+                            <ListSubheader>{t("pages.metabuilds.vsElement.Blaze")}</ListSubheader>
 
+                            <Stack
+                                spacing={2}
+                                sx={{ mt: 2 }}
+                            >
+                                {buildsByElement[ElementalType.Blaze].map((build, index) =>
+                                    renderBuild(index, build.buildId, build.title),
+                                )}
+                            </Stack>
+                        </>
+                    )}
+
+                    {buildsByElement[ElementalType.Frost].length > 0 && (
+                        <>
+                            <ListSubheader>{t("pages.metabuilds.vsElement.Frost")}</ListSubheader>
+
+                            <Stack
+                                spacing={2}
+                                sx={{ mt: 2 }}
+                            >
+                                {buildsByElement[ElementalType.Frost].map((build, index) =>
+                                    renderBuild(index, build.buildId, build.title),
+                                )}
+                            </Stack>
+                        </>
+                    )}
+
+                    {buildsByElement[ElementalType.Terra].length > 0 && (
+                        <>
+                            <ListSubheader>{t("pages.metabuilds.vsElement.Terra")}</ListSubheader>
+
+                            <Stack
+                                spacing={2}
+                                sx={{ mt: 2 }}
+                            >
+                                {buildsByElement[ElementalType.Terra].map((build, index) =>
+                                    renderBuild(index, build.buildId, build.title),
+                                )}
+                            </Stack>
+                        </>
+                    )}
+
+                    {buildsByElement[ElementalType.Shock].length > 0 && (
+                        <>
+                            <ListSubheader>{t("pages.metabuilds.vsElement.Shock")}</ListSubheader>
+
+                            <Stack
+                                spacing={2}
+                                sx={{ mt: 2 }}
+                            >
+                                {buildsByElement[ElementalType.Shock].map((build, index) =>
+                                    renderBuild(index, build.buildId, build.title),
+                                )}
+                            </Stack>
+                        </>
+                    )}
+
+                    {buildsByElement[ElementalType.Radiant].length > 0 && (
+                        <>
+                            <ListSubheader>{t("pages.metabuilds.vsElement.Radiant")}</ListSubheader>
+
+                            <Stack
+                                spacing={2}
+                                sx={{ mt: 2 }}
+                            >
+                                {buildsByElement[ElementalType.Radiant].map((build, index) =>
+                                    renderBuild(index, build.buildId, build.title),
+                                )}
+                            </Stack>
+                        </>
+                    )}
+
+                    {buildsByElement[ElementalType.Umbral].length > 0 && (
+                        <>
+                            <ListSubheader>{t("pages.metabuilds.vsElement.Umbral")}</ListSubheader>
+
+                            <Stack
+                                spacing={2}
+                                sx={{ mt: 2 }}
+                            >
+                                {buildsByElement[ElementalType.Umbral].map((build, index) =>
+                                    renderBuild(index, build.buildId, build.title),
+                                )}
+                            </Stack>
+                        </>
+                    )}
+
+                    {buildsByElement["None"].length > 0 && (
                         <Stack
                             spacing={2}
                             sx={{ mt: 2 }}
                         >
-                            {buildsByElement[ElementalType.Blaze].map((build, index) =>
+                            {buildsByElement["None"].map((build, index) =>
                                 renderBuild(index, build.buildId, build.title),
                             )}
                         </Stack>
-                    </>
-                )}
-
-                {buildsByElement[ElementalType.Frost].length > 0 && (
-                    <>
-                        <ListSubheader>{t("pages.metabuilds.vsElement.Frost")}</ListSubheader>
-
-                        <Stack
-                            spacing={2}
-                            sx={{ mt: 2 }}
-                        >
-                            {buildsByElement[ElementalType.Frost].map((build, index) =>
-                                renderBuild(index, build.buildId, build.title),
-                            )}
-                        </Stack>
-                    </>
-                )}
-
-                {buildsByElement[ElementalType.Terra].length > 0 && (
-                    <>
-                        <ListSubheader>{t("pages.metabuilds.vsElement.Terra")}</ListSubheader>
-
-                        <Stack
-                            spacing={2}
-                            sx={{ mt: 2 }}
-                        >
-                            {buildsByElement[ElementalType.Terra].map((build, index) =>
-                                renderBuild(index, build.buildId, build.title),
-                            )}
-                        </Stack>
-                    </>
-                )}
-
-                {buildsByElement[ElementalType.Shock].length > 0 && (
-                    <>
-                        <ListSubheader>{t("pages.metabuilds.vsElement.Shock")}</ListSubheader>
-
-                        <Stack
-                            spacing={2}
-                            sx={{ mt: 2 }}
-                        >
-                            {buildsByElement[ElementalType.Shock].map((build, index) =>
-                                renderBuild(index, build.buildId, build.title),
-                            )}
-                        </Stack>
-                    </>
-                )}
-
-                {buildsByElement[ElementalType.Radiant].length > 0 && (
-                    <>
-                        <ListSubheader>{t("pages.metabuilds.vsElement.Radiant")}</ListSubheader>
-
-                        <Stack
-                            spacing={2}
-                            sx={{ mt: 2 }}
-                        >
-                            {buildsByElement[ElementalType.Radiant].map((build, index) =>
-                                renderBuild(index, build.buildId, build.title),
-                            )}
-                        </Stack>
-                    </>
-                )}
-
-                {buildsByElement[ElementalType.Umbral].length > 0 && (
-                    <>
-                        <ListSubheader>{t("pages.metabuilds.vsElement.Umbral")}</ListSubheader>
-
-                        <Stack
-                            spacing={2}
-                            sx={{ mt: 2 }}
-                        >
-                            {buildsByElement[ElementalType.Umbral].map((build, index) =>
-                                renderBuild(index, build.buildId, build.title),
-                            )}
-                        </Stack>
-                    </>
-                )}
-
-                {buildsByElement["None"].length > 0 && (
-                    <Stack
-                        spacing={2}
-                        sx={{ mt: 2 }}
-                    >
-                        {buildsByElement["None"].map((build, index) => renderBuild(index, build.buildId, build.title))}
-                    </Stack>
-                )}
-            </>
-        );
-    };
+                    )}
+                </>
+            );
+        },
+        [renderBuild, t],
+    );
 
     const renderSubcategories = (buildCategory: BuildCategory) => (
         <>
